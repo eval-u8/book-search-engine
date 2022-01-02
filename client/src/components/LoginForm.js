56524db1261ea3f1daad57e18141173cae8841f2
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-02 16:25:13 -0600

    same thing as signup js but with login js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,11 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from "@apollo/client";
 
-import { loginUser } from '../utils/API';
+
+// import { loginUser } from '../utils/API';
+import { LOGIN_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,15 +29,10 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
