0a3f1cd1ad6ad7ecfb50cedea3ea06890532d932
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-02 17:58:45 -0600

    trying to fix signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -15,12 +15,12 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  const [addUser, { error }] = useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
-
-  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
