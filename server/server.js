33f2687c8800c6773f801abcc843ca843ce1e6d0
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-02 18:18:31 -0600

    managed to make the login work

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {authMiddleware} = require('./utils/auth');
 
 
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
